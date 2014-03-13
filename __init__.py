1787e83a07b6adbd4406aae66da6e70683192896
Gabriela <gabicavalcantesilva@gmail.com>
2014-03-13 20:46:51 -0300

    Add code

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"DB": "pyladies_blog"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
+
+def register_blueprints(app):
+    # Prevents circular imports
+    from pyladiesblog.views import posts
+    app.register_blueprint(posts)
+
+register_blueprints(app)
+
+if __name__ == '__main__':
+	app.run()

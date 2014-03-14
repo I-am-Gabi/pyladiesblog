be0e13c5e78b2d37dfe54b4e9bf6afe781b07216
Gabriela <gabicavalcantesilva@gmail.com>
2014-03-13 22:05:29 -0300

    add import heroku

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from flask.ext.heroku import Heroku
 from flask.ext.script import Manager, Server
 from pyladiesblog import app
 
